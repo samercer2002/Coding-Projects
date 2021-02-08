56ab44db5cf44816270fa5e951d91c16e0dbea60
samercer2002 <70397624+samercer2002@users.noreply.github.com>
2021-02-08 18:25:04 -0500

    Add files via upload

---
+++ Personal Projects/Procedural Generation in Unity/Data/TextureData.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Linq;
+
+[CreateAssetMenu()]
+public class TextureData : UpdateableData
+{
+    const int textureSize = 512;
+    const TextureFormat textureFormat = TextureFormat.RGB565;
+
+    public Layer[] layers;
+
+    float savedMinHeight;
+    float savedMaxHeight;
+
+    public void ApplyToMaterial(Material material)
+    {
+
+
+        UpdateMeshHeights(material, savedMinHeight, savedMaxHeight);
+    }
+
+    public void UpdateMeshHeights(Material material, float minHeight, float maxHeight)
+    {
+        savedMinHeight = minHeight;
+        savedMaxHeight = maxHeight;
+
+        material.SetFloat("minHeight", minHeight);
+        material.SetFloat("maxHeight", maxHeight);
+
+        material.SetFloatArray("baseBlends", layers.Select(x => x.blendStrength).ToArray());
+        material.SetInt("layerCount", layers.Length);
+        material.SetColorArray("baseColors", layers.Select(x => x.tint).ToArray());
+        material.SetFloatArray("baseColorStrength", layers.Select(x => x.tintStrength).ToArray());
+        material.SetFloatArray("baseStartHeights", layers.Select(x => x.startHeight).ToArray());
+        material.SetFloatArray("baseTextureScales", layers.Select(x => x.textureScale).ToArray());
+        Texture2DArray textureArray = GenerateTextureArray(layers.Select(x => x.texture).ToArray());
+        material.SetTexture("baseTextures", textureArray);
+    }
+
+
+    Texture2DArray GenerateTextureArray(Texture2D[] textures)
+    {
+        Texture2DArray textureArray = new Texture2DArray(textureSize, textureSize, textures.Length, textureFormat, true);
+        for(int i = 0; i < textures.Length; i++)
+        {
+            textureArray.SetPixels(textures[i].GetPixels(), i);
+
+        }
+        textureArray.Apply();
+        return textureArray;
+    }
+
+    [System.Serializable]
+    public class Layer
+    {
+        public Texture2D texture;
+        public Color tint;
+        [Range(0, 1)]
+        public float tintStrength;
+        [Range(0, 1)]
+        public float blendStrength;
+        [Range(0, 1)]
+        public float startHeight;
+        public float textureScale;
+    }
+}
