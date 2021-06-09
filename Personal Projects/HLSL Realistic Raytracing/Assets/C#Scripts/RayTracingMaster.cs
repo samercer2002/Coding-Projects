f873448688cac973cb4bd8c7c7166eeb9ad251b9
samercer2002 <70397624+samercer2002@users.noreply.github.com>
2021-06-09 15:08:53 -0400

    HLSL Raytracing for more realistic materials.
    Renders spheres and any mesh-based object. Should only be used with lower poly object as it isn't optimized for high-poly meshes. To change the mesh object texture, look into the ShaderText.compute file for manual changes.

---
+++ Personal Projects/HLSL Realistic Raytracing/Assets/C#Scripts/RayTracingMaster.cs
@@ -0,0 +1,342 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RayTracingMaster : MonoBehaviour
+{
+    // Struct containing sphere information
+    struct Sphere
+    {
+        public Vector3 position;
+        public float radius;
+        public Vector3 albedo;
+        public Vector3 specular;
+        public float smoothness;
+        public Vector3 emission;
+    };
+
+    // Struct containing mesh object information
+    struct MeshObject
+    {
+        public Matrix4x4 localToWorldMatrix;
+        public int indices_offset;
+        public int indices_count;
+    };
+
+    // A compute shader variable that gets assigned in Unity
+    public ComputeShader RayTracingShader;
+    // A render texture variable given a target
+    private RenderTexture _target;
+    // Gets skybox texture
+    public Texture SkyboxTexture;
+    // Camera variable
+    private Camera _camera;
+    private float _lastFieldOfView;
+
+    // Light object variable
+    public Light DirectionalLight;
+
+    private uint _currentSample = 0;
+    // Material variable
+    private Material _addMaterial;
+
+    // Render Texture variable
+    private RenderTexture _converged;
+
+    // Seed variable
+    public int SphereSeed;
+
+    // List of transforms to watch
+    private List<Transform> _transformsToWatch = new List<Transform>();
+
+    // Light refraction variable
+    [Range(1, 8)]
+    public int refractions;
+
+    // Sphere radius range variable
+    public Vector2 SphereRadius = new Vector2(3.0f, 8.0f);
+    // Sphere placement amount and range
+    public uint SpheresMax = 100;
+    public float SpherePlacementRadius = 100.0f;
+    // Compute Buffer variable for spheres
+    private ComputeBuffer _sphereBuffer;
+
+    // Static bool for determining if the mesh object needs rebuilding
+    private static bool _meshObjectsNeedRebuilding = false;
+    // Static List variables for objects
+    private static List<RayTracingObject> _rayTracingObjects = new List<RayTracingObject>();
+    private static List<MeshObject> _meshObjects = new List<MeshObject>();
+    private static List<Vector3> _vertices = new List<Vector3>();
+    private static List<int> _indices = new List<int>();
+
+    // Mesh Object-based buffer variables
+    private ComputeBuffer _meshObjectBuffer;
+    private ComputeBuffer _vertexBuffer;
+    private ComputeBuffer _indexBuffer;
+
+    // Called at the execution of the game
+    private void Awake()
+    {
+        // Gets and sets the camera object
+        _camera = GetComponent<Camera>();
+
+        // Adds a transform to watch for the camera and light objects
+        _transformsToWatch.Add(transform);
+        _transformsToWatch.Add(DirectionalLight.transform);
+    }
+
+    private void Update()
+    {
+        // If the field of view changes
+        if (_camera.fieldOfView != _lastFieldOfView)
+        {
+            // Reset current sample
+            _currentSample = 0;
+            // Set last FOV to the current
+            _lastFieldOfView = _camera.fieldOfView;
+        }
+
+        // Foreach transform to watch
+        foreach (Transform t in _transformsToWatch)
+        {
+            // See if it has changed
+            if (t.hasChanged)
+            {
+                // Reset current sample
+                _currentSample = 0;
+                // Set to false
+                t.hasChanged = false;
+            }
+        }
+    }
+
+    // Register a mesh object
+    public static void RegisterObject(RayTracingObject obj)
+    {
+        // Add to the list and rebuild
+        _rayTracingObjects.Add(obj);
+        _meshObjectsNeedRebuilding = true;
+    }
+
+    // Remove a mesh object
+    public static void UnregisterObject(RayTracingObject obj)
+    {
+        // Remove from list and rebuild
+        _rayTracingObjects.Remove(obj);
+        _meshObjectsNeedRebuilding = true;
+    }
+
+    // Sets the shader parameters for the compute shader
+    private void SetShaderParameters()
+    {
+        RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
+        RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
+        RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
+        RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
+        RayTracingShader.SetInt("_Refractions", refractions);
+        Vector3 l = DirectionalLight.transform.forward;
+        RayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
+        RayTracingShader.SetBuffer(0, "_Spheres", _sphereBuffer);
+        RayTracingShader.SetFloat("_Seed", Random.value);
+        SetComputeBuffer("_Spheres", _sphereBuffer);
+        SetComputeBuffer("_MeshObjects", _meshObjectBuffer);
+        SetComputeBuffer("_Vertices", _vertexBuffer);
+        SetComputeBuffer("_Indices", _indexBuffer);
+    }
+
+    // Called by Unity after the camera has finished rendering
+    // Intakes a source and destination (two render textures)
+    // Calls a render method using the destination
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        RebuildMeshObjectBuffers();
+        SetShaderParameters();
+        Render(destination);
+    }
+
+    // Private method used by the OnRenderImage method
+    // Intakes a destination
+    private void Render(RenderTexture destination)
+    {
+        // Make sure we have a current render target
+        InitRenderTexture();
+        // Set the target and dispatch the compute shader
+        RayTracingShader.SetTexture(0, "Result", _target);
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        RayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+        // Blit the result texture to the screen
+        if (_addMaterial == null)
+            _addMaterial = new Material(Shader.Find("Hidden/AddShader"));
+        _addMaterial.SetFloat("_Sample", _currentSample);
+        Graphics.Blit(_target, _converged, _addMaterial);
+        Graphics.Blit(_converged, destination);
+        _currentSample++;
+    }
+    // Called by the Render method
+    private void InitRenderTexture()
+    {
+        // Sees if the target is smaller than the screen or if it doesn't exist
+        if (_target == null || _target.width != Screen.width || _target.height != Screen.height)
+        {
+            // Release render texture if we already have one
+            if (_target != null)
+            {
+                _target.Release();
+                _converged.Release();
+            }
+
+            // Get a render target for Ray Tracing
+            _target = new RenderTexture(Screen.width, Screen.height, 0,
+                RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _target.enableRandomWrite = true;
+            _target.Create();
+            _converged = new RenderTexture(Screen.width, Screen.height, 0,
+                RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _converged.enableRandomWrite = true;
+            _converged.Create();
+
+            // Reset sampling
+            _currentSample = 0;
+        }
+    }
+
+    // Sets up the scene on enable
+    private void OnEnable()
+    {
+        _currentSample = 0;
+        SetUpScene();
+    }
+    // Releases buffers on disable
+    private void OnDisable()
+    {
+        _sphereBuffer?.Release();
+        _meshObjectBuffer?.Release();
+        _vertexBuffer?.Release();
+        _indexBuffer?.Release();
+    }
+
+    // Creates the compute buffer
+    private static void CreateComputeBuffer<T>(ref ComputeBuffer buffer, List<T> data, int stride)
+    where T : struct
+    {
+        // Do we already have a compute buffer?
+        if (buffer != null)
+        {
+            // If no data or buffer doesn't match the given criteria, release it
+            if (data.Count == 0 || buffer.count != data.Count || buffer.stride != stride)
+            {
+                buffer.Release();
+                buffer = null;
+            }
+        }
+        if (data.Count != 0)
+        {
+            // If the buffer has been released or wasn't there to
+            // begin with, create it
+            if (buffer == null)
+            {
+                buffer = new ComputeBuffer(data.Count, stride);
+            }
+            // Set data on the buffer
+            buffer.SetData(data);
+        }
+    }
+    // Sets the compute buffer
+    private void SetComputeBuffer(string name, ComputeBuffer buffer)
+    {
+        if (buffer != null)
+        {
+            RayTracingShader.SetBuffer(0, name, buffer);
+        }
+    }
+
+    // Sets up the spheres for the scene
+    private void SetUpScene()
+    {
+        Random.InitState(SphereSeed);
+        List<Sphere> spheres = new List<Sphere>();
+        // Add a number of random spheres
+        for (int i = 0; i < SpheresMax; i++)
+        {
+            Sphere sphere = new Sphere();
+            // Radius and radius
+            sphere.radius = SphereRadius.x + Random.value * (SphereRadius.y - SphereRadius.x);
+            Vector2 randomPos = Random.insideUnitCircle * SpherePlacementRadius;
+            sphere.position = new Vector3(randomPos.x, sphere.radius, randomPos.y);
+            // Reject spheres that are intersecting others
+            foreach (Sphere other in spheres)
+            {
+                float minDist = sphere.radius + other.radius;
+                if (Vector3.SqrMagnitude(sphere.position - other.position) < minDist * minDist)
+                    goto SkipSphere;
+            }
+            // Albedo and specular color
+            Color color = Random.ColorHSV();
+            float chance = Random.value;
+
+            bool metal = chance < 0.4f;
+            sphere.albedo = metal ? Vector3.zero : new Vector3(color.r, color.g, color.b);
+            sphere.specular = metal ? new Vector3(color.r, color.g, color.b) : new Vector3(0.04f, 0.04f, 0.04f);
+            sphere.smoothness = Random.value;
+
+            if (chance < 0.25f)
+            {
+                Color emission = Random.ColorHSV(0, 1, 0, 1, 0.0f, 5.0f);
+                sphere.emission = new Vector3(emission.r, emission.g, emission.b);
+            }
+
+            // Add the sphere to the list
+            spheres.Add(sphere);
+        SkipSphere:
+            continue;
+        }
+        // Assign to compute buffer
+        if (_sphereBuffer != null)
+            _sphereBuffer.Release();
+        if (spheres.Count > 0)
+        {
+            _sphereBuffer = new ComputeBuffer(spheres.Count, 56);
+            _sphereBuffer.SetData(spheres);
+        }
+    }
+
+    // Rebuilds the mesh object buffers
+    private void RebuildMeshObjectBuffers()
+    {
+        if (!_meshObjectsNeedRebuilding)
+        {
+            return;
+        }
+        _meshObjectsNeedRebuilding = false;
+        _currentSample = 0;
+        // Clear all lists
+        _meshObjects.Clear();
+        _vertices.Clear();
+        _indices.Clear();
+        // Loop over all objects and gather their data
+        foreach (RayTracingObject obj in _rayTracingObjects)
+        {
+            Mesh mesh = obj.GetComponent<MeshFilter>().sharedMesh;
+            // Add vertex data
+            int firstVertex = _vertices.Count;
+            _vertices.AddRange(mesh.vertices);
+            // Add index data - if the vertex buffer wasn't empty before, the
+            // indices need to be offset
+            int firstIndex = _indices.Count;
+            var indices = mesh.GetIndices(0);
+            _indices.AddRange(indices.Select(index => index + firstVertex));
+            // Add the object itself
+            _meshObjects.Add(new MeshObject()
+            {
+                localToWorldMatrix = obj.transform.localToWorldMatrix,
+                indices_offset = firstIndex,
+                indices_count = indices.Length
+            });
+        }
+        CreateComputeBuffer(ref _meshObjectBuffer, _meshObjects, 72);
+        CreateComputeBuffer(ref _vertexBuffer, _vertices, 12);
+        CreateComputeBuffer(ref _indexBuffer, _indices, 4);
+    }
+}
---
+++ Personal Projects/HLSL Realistic Raytracing/Assets/C#Scripts/RayTracingObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class RayTracingObject : MonoBehaviour
+{
+    // Registers the mesh object if enabled
+    private void OnEnable()
+    {
+        RayTracingMaster.RegisterObject(this);
+    }
+    private void OnDisable()
+    {
+        RayTracingMaster.UnregisterObject(this);
+    }
+}
